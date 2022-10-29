4e890f88e6a6ecb7d02106de475bf4eee91cbaf5
Gopalakrishnan3008 <srinathgopalakrishnan200@gmail.com>
2022-10-29 12:15:33 +0530

    Task_1

---
+++ src/com/training/students.java
@@ -0,0 +1,34 @@
+package com.training;
+
+public class students {
+
+	private void name() {
+		System.out.println("Gopalakrishnan");
+	}
+
+	private void age() {
+		System.out.println(20);
+	}
+	private void place() {
+		System.out.println("chennai");
+
+	}
+	private void rollno() {
+		System.out.println("1913281040042");
+
+	}
+	private void state() {
+		System.out.println("tamilnadu");
+
+	}
+	public static void main(String[] args) {
+
+students s= new students();
+s.age();
+s.name();
+s.place();
+s.rollno();
+s.state();
+}
+	
+}
---
+++ src/nestied_forloop.java
@@ -0,0 +1,14 @@
+
+public class nestied_forloop {
+
+	public static void main(String[] args) {
+for (int i = 0; i <= 8; i++) {
+	for (int j = 0; j <=i ; j++) {
+		System.out.println("*");
+		
+	}
+	System.out.println();
+}
+	}
+
+}
